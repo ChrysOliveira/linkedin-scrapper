5e8ec1bc48d24ed519a9e1b3f3b643bda6d77081
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2024-09-19 20:44:02 -0300

    first commit

---
+++ src/main/java/com/example/linkedin_scrapper/LinkedinScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkedin_scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/linkedin_scrapper/LinkedinScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linkedin_scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
