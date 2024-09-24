52597032f295ec4baf5cf322dbe70f8271b3c783
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2024-09-24 10:14:45 -0300

    start

--- src/main/java/com/example/linkedin_scrapper/LinkedinScrapperApplication.java
+++ src/main/java/com/example/linkedin_scrapper/LinkedinScrapperApplication.java
@@ -1,13 +1,21 @@
 package com.example.linkedin_scrapper;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LinkedinScrapperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LinkedinScrapperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LinkedinScrapperApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner demo() {
+        return (args) -> {
+            System.out.println("Hello World!");
+        };
+    }
 }
